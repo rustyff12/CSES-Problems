d3de5fdc07e95611d353bf9fca59976120159e80
Russell <75209707+rustyff12@users.noreply.github.com>
2025-07-17 16:48:03 +0200

    moved to submitted

--- 1-WeirdAlgorithm/main.py
+++ 1-WeirdAlgorithm/main.py
@@ -1,6 +1,6 @@
-test_1 = 3
-test_2 = 4
-test_3 = 37
+# test_1 = 3
+# test_2 = 4
+# test_3 = 37
 
 def collatz(num, result = ""):
 	if num == 1:
@@ -19,9 +19,9 @@ def collatz(num, result = ""):
 
 	
 
-print(collatz(test_1)) # 3 10 5 16 8 4 2 1
-print(collatz(test_2)) # 4 2 1
-print(collatz(test_3)) # 37 112 56 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+# print(collatz(test_1)) # 3 10 5 16 8 4 2 1
+# print(collatz(test_2)) # 4 2 1
+# print(collatz(test_3)) # 37 112 56 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
 
 
 
